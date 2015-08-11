e9fa31c9c71a28d2aea3713cc4e3a582e124f8a6
alexbuicescu <alexbuicescu@gmail.com>
2015-08-12 02:31:05 +0300

    rendering works now

--- Main/src/LibGdxExample/Main.java
+++ Main/src/LibGdxExample/Main.java
@@ -1,9 +1,47 @@
 package LibGdxExample;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Main extends Game {
+    private SpriteBatch batch;
+    private BitmapFont font;
+
     @Override
     public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.setColor(Color.RED);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+//        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        font.draw(batch, "Hello World", 200, 200);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
     }
 }
